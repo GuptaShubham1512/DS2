62cc0f2eb6b6a5f53b13638ca36c2689ac983665
GuptaShubham1512 <148928391+GuptaShubham1512@users.noreply.github.com>
2024-05-17 19:56:19 +0530

    Add files via upload

---
+++ prepostarthematic.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[2]={1,19};
+    int *ptr=&arr[0];
+    //derefrencing
+    cout<<(*ptr)++<<"\n";
+
+    cout<<arr[0]<<" "<<arr[1]<<"\n";
+    cout<<ptr<<" "<<*ptr<<"\n";
+    return 0;
+
+}
+
+//result
+//1
+//2 19
+//address 2
