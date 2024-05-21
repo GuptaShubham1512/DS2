8e1e319d17273965089856dcc1672ab247758104
GuptaShubham1512 <148928391+GuptaShubham1512@users.noreply.github.com>
2024-05-21 22:31:48 +0530

    Add files via upload

---
+++ Revesion1.cpp
@@ -0,0 +1,154 @@
+/*#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a,b;
+    cin>>a>>b;
+    cout<<a+b;
+    return 0;
+}*/
+
+
+//ADDING NUMBER BY MENTIONING IT
+
+
+/*#include<iostream>
+using namespace std;
+ int main(){
+    int a,b;
+    cout<<"Enter the first nmber:";
+    cin>>a;
+
+    cout<<"Enter the second number:";
+    cin>>b;
+
+    cout<<a+b;
+
+    return 0;
+ }*/
+
+
+// Asciii value :A=65  B=66
+            // a=98     b=99
+
+
+
+//  SWAPPING OF A NUMBER
+
+/*#include<iostream>
+using namespace std;
+ int main(){
+    int n,m;
+    cin>>n>>m;
+
+    cout<<"value of n:"<<n<<endl;
+    cout<<"value of m:"<<m<<endl;
+     int temp;
+     temp=n;
+     n=m;
+     m=temp;
+
+    cout<<"value of n:"<<n<<endl;
+    cout<<"value of m:"<<m<<endl;
+
+    return 0;
+    
+
+ }*/
+
+
+ // SWITCH CASE
+
+/*#include<iostream>
+using namespace std;
+
+int main(){
+    char alphabet;
+    cout<<"Enter the alphabet:";
+    cin>>alphabet;
+
+    switch(alphabet){
+        case'a':
+        cout<<"It is a vowel"<<endl;
+        break;
+
+        case'e':
+        cout<<"It is a vowel"<<endl;
+        break;
+
+        case'i':
+        cout<<"It is a vowel"<<endl;
+        break;
+
+        case'o':
+        cout<<"It is a vowel"<<endl;
+        break;
+
+
+        case'u':
+        cout<<"It is a vowel"<<endl;
+        break;
+
+        default:
+        cout<<"it is a consonant"<<endl;
+    }
+
+}*/
+
+//DESIGNING A CALCULATOR(+,-,*,/,%)
+
+/*#include<iostream>
+using namespace std;
+
+int main(){
+
+int n1,n2;
+cout<<"Enter 2 integers";
+cin>>n1>>n2;
+
+cout<<"Enter the operators:";
+
+char op;
+cin>>op;
+
+switch(op){
+    case'+':
+    cout<<"Sum is:"<<n1+n2<<endl;
+    break;
+
+    case'-':
+    cout<<"Differnce is:"<<n1-n2<<endl;
+    break;
+
+    case'*':
+    cout<<"Multiplcation is:"<<n1*n2<<endl;
+    break;
+
+    
+
+    }
+
+
+
+
+}*/
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello shubham";
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
