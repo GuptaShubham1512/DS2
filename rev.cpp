46c31f62928a628e4922efcdf2d5b8ffabc50031
GuptaShubham1512 <148928391+GuptaShubham1512@users.noreply.github.com>
2024-05-21 22:31:30 +0530

    Add files via upload

---
+++ rev.cpp
@@ -0,0 +1,41 @@
+//DESIGNING A CALCULATOR(+,-,*,/,%)
+
+/*#include<iostream>
+using namespace std;
+
+int main(){
+
+int n1,n2;
+cout<<"Enter 2 integers";
+cin>>n1>>n2;
+
+cout<<"Enter the operators:";
+
+char op;
+cin>>op;
+
+switch(op){
+    case'+':
+    cout<<"Sum is:"<<n1+n2<<endl;
+    break;
+
+    case'-':
+    cout<<"Differnce is:"<<n1-n2<<endl;
+    break;
+
+    case'*':
+    cout<<"Multiplcation is:"<<n1*n2<<endl;
+    break;
+
+    }
+
+}*/
+
+//
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+}
