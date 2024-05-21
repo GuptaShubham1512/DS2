f4c3f52bbc53aa0f5b2274e21acc3ae2ce42a78b
GuptaShubham1512 <148928391+GuptaShubham1512@users.noreply.github.com>
2024-05-21 22:32:07 +0530

    Add files via upload

---
+++ Rev3.cpp
@@ -0,0 +1,29 @@
+/*#include<iostream>
+using namespace std;
+
+int main(){
+    int  n,m;
+    cin>>n>>m;
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+
+            if(row=1|| row=n|| column=1|| column=m)
+            cout<<"*";
+            
+        }
+        else{
+            cout<<"  ";
+
+        }
+        cout<<endl;
+    }
+}
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+
+}
